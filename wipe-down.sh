12fbbe278ff5443da45680863079dd84d62b653b
Matt Day <mattsday@gmail.com>
2018-05-25 09:21:34 +0100

    Initial commit

---
+++ wipe-down.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Input checking
+if [ -z ${PAS_ADMIN_USER} ]; then echo NO PAS_ADMIN_USER; exit 1; fi
+if [ -z ${PAS_ADMIN_PW} ]; then echo NO_PAS_ADMIN_PW; exit 1; fi
+if [ -z ${PAS_SAFE_ORGS} ]; then echo NO_PAS_SAFE_ORGS; exit 1; fi
+if [ -z ${PAS_SAFE_SPACES} ]; then echo NO_PAS_SAFE_SPACES; exit 1; fi
+
+# Login to CF in the system org/space
+cf login -u ${PAS_ADMIN_USER} -p ${PAS_ADMIN_PW} -o system -s system
+
+CF_ORGS=$(cf orgs | tail +4)
+PAS_SAFE_ORGS=$(echo ${PAS_SAFE_ORGS} | sed 's/,/ /g')
+PAS_SAFE_SPACES=$(echo ${PAS_SAFE_SPACES} | sed 's/,/ /g')
+
+
+for org in ${CF_ORGS}; do
+	# NEVER wipe the system org, ignore config
+    if [ "{$org}" = "system" ]; then
+        continue
+    fi
+	if [[ ${PAS_SAFE_ORGS} =~ (^|[[:space:]])${org}($|[[:space:]]) ]]; then
+		echo $org is safe
+		continue;
+	fi
+	cf target -o $org >/dev/null 2>&1
+    ORG_SPACES=$(cf spaces | tail +4)
+    for space in $ORG_SPACES; do
+		if [[ ${PAS_SAFE_SPACES} =~ (^|[[:space:]])${space}($|[[:space:]]) ]]; then
+			echo $space in $org is safe
+			continue;
+		fi
+        cf target -o $org -s $space >/dev/null 2>&1
+        CF_APPS=$(cf apps | tail +5 | awk '{print $1}')
+        echo Deleting all apps in org $org space $space
+        for app in $CF_APPS; do
+			case $app in
+				*-keep)
+					;;
+				*)
+					cf delete -f $app
+					;;
+			esac
+        done
+		echo Deleting all services in org $org space $space
+		CF_SERVICES=$(cf services | tail +5 | awk '{print $1}')
+		for service in $CF_SERVICES; do
+			case $service in
+				*-keep)
+					;;
+				*)
+					cf delete-service -f $service
+					;;
+			esac
+		done
+		echo Clearing up routes
+		cf routes | tail +4 | while read -r line; do
+			ROUTE_APP=$(echo $line | awk '{print $4}')
+			if [ -z "$ROUTE_APP" ]; then 
+				cf delete-route -f --hostname $(echo $line | awk '{ print $2 }') $(echo $line | awk '{ print $3 }')
+			fi
+		done
+    done
+done
