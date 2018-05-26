0891e74c2c667ff0c9aa58c7e056bdec026c8bc1
Matt Day <mattsday@gmail.com>
2018-05-26 09:20:58 +0100

    Fixed script for gnu tail

--- wipe-down.sh
+++ wipe-down.sh
@@ -9,7 +9,7 @@ if [ -z ${PAS_API_ADDRESS} ]; then echo NO PAS_API_ADDRESS; exit 1; fi
 # Login to CF in the system org/space
 cf login -a ${PAS_API_ADDRESS} -u ${PAS_ADMIN_USER} -p ${PAS_ADMIN_PW} -o system -s system 
 
-CF_ORGS=$(cf orgs | tail +4)
+CF_ORGS=$(cf orgs | tail -n +4)
 PAS_SAFE_ORGS=$(echo ${PAS_SAFE_ORGS} | sed 's/,/ /g')
 PAS_SAFE_SPACES=$(echo ${PAS_SAFE_SPACES} | sed 's/,/ /g')
 
@@ -24,14 +24,14 @@ for org in ${CF_ORGS}; do
 		continue;
 	fi
 	cf target -o $org >/dev/null 2>&1
-    ORG_SPACES=$(cf spaces | tail +4)
+    ORG_SPACES=$(cf spaces | tail -n +4)
     for space in $ORG_SPACES; do
 		if [[ ${PAS_SAFE_SPACES} =~ (^|[[:space:]])${space}($|[[:space:]]) ]]; then
 			echo $space in $org is safe
 			continue;
 		fi
         cf target -o $org -s $space >/dev/null 2>&1
-        CF_APPS=$(cf apps | tail +5 | awk '{print $1}')
+        CF_APPS=$(cf apps | tail -n +5 | awk '{print $1}')
         echo Deleting all apps in org $org space $space
         for app in $CF_APPS; do
 			case $app in
@@ -43,7 +43,7 @@ for org in ${CF_ORGS}; do
 			esac
         done
 		echo Deleting all services in org $org space $space
-		CF_SERVICES=$(cf services | tail +5 | awk '{print $1}')
+		CF_SERVICES=$(cf services | tail -n +5 | awk '{print $1}')
 		for service in $CF_SERVICES; do
 			case $service in
 				*-keep)
@@ -54,7 +54,7 @@ for org in ${CF_ORGS}; do
 			esac
 		done
 		echo Clearing up routes
-		cf routes | tail +4 | while read -r line; do
+		cf routes | tail -n +4 | while read -r line; do
 			ROUTE_APP=$(echo $line | awk '{print $4}')
 			if [ -z "$ROUTE_APP" ]; then 
 				cf delete-route -f --hostname $(echo $line | awk '{ print $2 }') $(echo $line | awk '{ print $3 }')
